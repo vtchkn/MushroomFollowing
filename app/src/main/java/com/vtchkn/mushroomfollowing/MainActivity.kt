31b3ac827ef3e57cb41118c2b89eb6dfaf78e78d
Varvara Solovyeva <varvara.solovyeva@carsales.com.au>
2021-01-07 00:23:38 -0300

    Auth temproraily not requiered

--- app/src/main/java/com/vtchkn/mushroomfollowing/MainActivity.kt
+++ app/src/main/java/com/vtchkn/mushroomfollowing/MainActivity.kt
@@ -1,28 +1,22 @@
 package com.vtchkn.mushroomfollowing
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.actionCodeSettings
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
 
-    val actionCodeSettings = actionCodeSettings {
-        // URL you want to redirect back to. The domain (www.example.com) for this
-        // URL must be whitelisted in the Firebase Console.
-        url = "https://mushroomfollowing.firebaseapp.com"
-        // This must be true
-        handleCodeInApp = true
-        iosBundleId = "com.example.ios"
-        setAndroidPackageName(
-            "com.example.android",
-            true, /* installIfNotAvailable */
-            "12" /* minimumVersion */)
+    companion object {
+        const val TAG = "MainActivity"
     }
 
     private val viewModel: MainViewModel by lazy {
@@ -36,14 +30,69 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.fetch()
 
+        FirebaseDynamicLinks.getInstance()
+            .getDynamicLink(intent)
+            .addOnSuccessListener(this) { pendingDynamicLinkData ->
+                // Get deep link from result (may be null if no link is found)
+                var deepLink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    deepLink = pendingDynamicLinkData.link
+                }
+
+                // Handle the deep link. For example, open the linked
+                // content, or apply promotional credit to the user's
+                // account.
+                // ...
+
+                // [START_EXCLUDE]
+                // Display deep link in the UI
+                if (deepLink != null) {
+                    Snackbar.make(
+                        findViewById(android.R.id.content),
+                        "Found deep link!", Snackbar.LENGTH_LONG
+                    ).show()
+
+                    val auth = Firebase.auth
+                    val intent = intent
+                    val emailLink = intent.data.toString()
+
+// Confirm the link is a sign-in with email link.
+                    if (auth.isSignInWithEmailLink(emailLink)) {
+                        // Retrieve this from wherever you stored it
+                        val email = "zhhhh11@gmail.com"
+
+                        // The client SDK will parse the code from the link for you.
+                        auth.signInWithEmailLink(email, emailLink)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    Log.d(TAG, "Successfully signed in with email link!")
+                                    val result = task.result
+                                    // You can access the new user via result.getUser()
+                                    // Additional user info profile *not* available via:
+                                    // result.getAdditionalUserInfo().getProfile() == null
+                                    // You can check if the user is new or existing:
+                                    // result.getAdditionalUserInfo().isNewUser()
+                                    viewModel.fetch()
+                                } else {
+                                    Log.e(TAG, "Error signing in with email link", task.exception)
+                                }
+                            }
+                    } else {
+                        Log.d(TAG, "getDynamicLink: no link found")
+                    }
+                    // [END_EXCLUDE]
+                }
+
+
+            }
     }
+
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         updateUI(currentUser)
     }
-
 
 
     private fun updateUI(currentUser: FirebaseUser?) {
--- app/src/main/java/com/vtchkn/mushroomfollowing/MainViewModel.kt
+++ app/src/main/java/com/vtchkn/mushroomfollowing/MainViewModel.kt
@@ -2,21 +2,44 @@ package com.vtchkn.mushroomfollowing
 
 import android.app.Application
 import android.content.Intent
+import android.os.Build
+import android.util.Log
 import androidx.lifecycle.*
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.actionCodeSettings
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.vtchkn.mushroomfollowing.repository.MushroomFollowingRepository
 import com.vtchkn.mushroomfollowing.repository.model.MushroomGrowingEntity
 import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     val data = MutableLiveData<List<MushroomGrowingEntity>>()
+    val actionCodeSettings = actionCodeSettings {
+        // URL you want to redirect back to. The domain (www.example.com) for this
+        // URL must be whitelisted in the Firebase Console.
+        url = "https://mushroomfollowing.page.link/redirect"
+        // This must be true
+        handleCodeInApp = true
+        setAndroidPackageName(
+            application.packageName,
+            true, /* installIfNotAvailable */
+            Build.VERSION.SDK_INT.toString() /* minimumVersion */)
+    }
 
 
     fun fetch() {
         viewModelScope.launch {
-            if (isSignedIn()){
+//            if (isSignedIn()){
                 MushroomFollowingRepository().getMushroomGrowingEntities()
-            }
+//            } else {
+//                Firebase.auth.sendSignInLinkToEmail("zhhhh11@gmail.com", actionCodeSettings)
+//                    .addOnCompleteListener { task ->
+//                        if (task.isSuccessful) {
+//                            Log.d("MainViewModel", "Email sent.")
+//                        }
+//                    }
+//            }
         }
     }
 
@@ -32,6 +55,6 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     private fun isSignedIn(): Boolean {
-        return FirebaseAuth.getInstance().currentUser == null
+        return FirebaseAuth.getInstance().currentUser != null
     }
 }
